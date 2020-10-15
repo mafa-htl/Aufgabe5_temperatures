7a05ac590bb80064930631fe311407fe75a102ee
Matteo Falkenberg <matteo.falkenberg@htl-wels.at>
2020-10-15 10:02:01 +0200

    initial structure

---
+++ src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+/**class Main
+ * @author Matteo Falkenberg
+ * @version 1.20, 15.10.2020
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+    
+}
